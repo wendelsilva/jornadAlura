35f39f3d074a27b644548621908d985ebdfabe53
wendel245 <48558013+wendel245@users.noreply.github.com>
2021-05-04 19:41:48 -0300

    add filter in table

--- Formação HTML e CSS/JavaScript/introducao-javascript/js/remover-paciente.js
+++ Formação HTML e CSS/JavaScript/introducao-javascript/js/remover-paciente.js
@@ -2,22 +2,24 @@ var pacientes = document.querySelectorAll(".paciente");
 
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(event){
+/* tabela.addEventListener("dblclick", function(event){
     var alvoEvento = event.target;
     var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover
     paiDoAlvo.remove();
 
-});
+}); */
 
-/* forma reduzida ou fatorada
+// forma reduzida ou fatorada
 
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-    event.target.parentNode.remove();
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500)
+    
 })
-
-*/
 
 
 /*pacientes.forEach(function(paciente){
