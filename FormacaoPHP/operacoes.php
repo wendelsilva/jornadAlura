a4f9dfcbac140bf1ee12edb217260366b4b5bdc4
wendel245 <48558013+wendel245@users.noreply.github.com>
2021-06-27 18:18:08 -0300

    refactor(mvc): modify files path

--- FormacaoPHP/operacoes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$idade = 21;
-$idadeDaqui10Anos = $idade + 10;
-$idadeHa5Anos = $idade - 5;
-
-echo $idadeDaqui10Anos;
-
-echo $idadeHa5Anos;
--- FormacaoPHP/variavel.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Simbolo do cifrão siginifica variável
-
-$idade = 22;
-$idade = 21;
-
-echo $idade;
