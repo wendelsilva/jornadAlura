480fd815b5ae5eee42c0ce65ea1bf9d79452a059
wendelsilva <wendel245.ws@gmail.com>
2021-09-22 21:42:19 -0300

    study(jspoo): first class in js poo

--- Formacao-Front-End/JavaScript/POO/index.js
+++ Formacao-Front-End/JavaScript/POO/index.js
@@ -1,11 +1,44 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente{
+    nome;
+    cpf;
+}
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 88822233309;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+class ContaCorrente{
+    agencia;
+    _saldo = 0;
 
-console.log(cliente1Nome)
+    sacar(valor) {
+        if(valor > this._saldo) {
+            console.log('Saldo insuficiente');
+            return;
+        }
+        this._saldo -= valor;
+    }
+
+    depositar(valor) {
+        if(valor > 0) {
+            this._saldo += valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ella";
+cliente1.cpf = 12322233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Wendel";
+cliente2.cpf = 11122233309;
+
+const contaCorrenteWendel = new ContaCorrente();
+contaCorrenteWendel.agencia = 1201;
+contaCorrenteWendel._saldo = 100;
+contaCorrenteWendel.depositar(100);
+
+const contaCorrenteElla = new ContaCorrente();
+contaCorrenteElla.agencia = 1301;
+contaCorrenteElla._saldo = 100;
+contaCorrenteElla.depositar(100);
+
+console.log(contaCorrenteWendel);
+console.log(contaCorrenteElla);
